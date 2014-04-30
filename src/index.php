b1343d1d512ace43e495151d8bc4afa0e1cce039
Patrick Forget <patforg@geekpad.ca>
2014-04-30 13:03:20 -0400

    added short desicription and styles

--- src/index.php
+++ src/index.php
@@ -1,11 +1,23 @@
 <?php
 
+
 /* validate that user cookie is set to the correct value */
 if ( !isset($_COOKIE['user']) || $_COOKIE['user'] !== 'loggedin' ) {
 
-        header("Location: login.php?message=". urlencode("You must login to enter protected area"));
-        exit;
+    header("location: login.php?message=". urlencode("You must login to enter protected area!"));
+    exit;
 
+} //if
+
+
+/* check if logout flag was set */
+if (isset($_GET['logout'])) {
+    setcookie("user","", (time() - 86400));
+    $_COOKIE['user'] = null;
+    unset($_COOKIE['user']);
+
+    header("location: login.php?message=". urlencode("You have been successfully logged out."));
+    exit;
 } //if
 
 ?>
@@ -13,9 +25,15 @@ if ( !isset($_COOKIE['user']) || $_COOKIE['user'] !== 'loggedin' ) {
 <html>
     <head>
         <meta charset="utf-8">
-        <title></title>
+        <title>PHPUnit and Selenium Demo - index page</title>
+        <link rel="stylesheet" href="./style.css">
     </head>
     <body>
         <h1>Welcome to protected area!</h1>
+        <div class="intro box">
+            <p>This is a demo page for the article <a href="http://geekpad.ca/blog/post/automating-browser-testing-with-phpunit-and-selenium" target="_blank">Integration Tests Using PHPUnit and Selenium</a></p>
+            <p>If you see this page you have logged in successfully.  A cookie named user has been set and simulates your authentication.</p>
+        </div>
+        <a href="?logout">logout</a>
     </body>
 </html>
--- src/login.php
+++ src/login.php
@@ -22,16 +22,18 @@ if (count($_POST) > 0) {
 <html>
     <head>
         <meta charset="utf-8">
-        <title></title>
-        <style>
-            .message {color: #900;}
-        </style>
+        <title>PHPUnit and Selenium Demo - Login page</title>
+        <link rel="stylesheet" href="./style.css">
     </head>
     <body>
         <form class="login" method="POST">
             <h1>Login Page</h1>
+            <div class="intro box">
+                <p>This is a demo page for the article <a href="http://geekpad.ca/blog/post/automating-browser-testing-with-phpunit-and-selenium" target="_blank">Integration Tests Using PHPUnit and Selenium</a></p>
+                <p>Use <strong>test</strong> for login and <strong>test</strong> for password to authenticate.</p>
+            </div>
             <?php if (strlen($message) > 0): ?>
-            <div class="message"><?php echo htmlspecialchars($message, ENT_QUOTES); ?></div>
+            <div class="message box"><?php echo htmlspecialchars($message, ENT_QUOTES); ?></div>
             <?php endif; ?>
             <div>
                 <label for="login">Login</label>
